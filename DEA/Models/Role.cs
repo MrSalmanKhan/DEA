d21e37c79c762394b64b7450d25b7e2755437c33
Asad Ishaq <mr.asadishaq10@gmail.com>
2018-04-20 12:03:46 +0500

    user login working

--- DEA/Models/Role.cs
+++ DEA/Models/Role.cs
@@ -18,6 +18,7 @@ namespace DEA.Models
         public Role()
         {
             this.Employees = new HashSet<Employee>();
+            this.Users = new HashSet<User>();
         }
     
         public int RoleID { get; set; }
@@ -25,5 +26,7 @@ namespace DEA.Models
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Employee> Employees { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<User> Users { get; set; }
     }
 }
